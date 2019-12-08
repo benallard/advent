e8d2b714536bb3ce8e2c4de3750006d66f1c2c9f
Benoît Allard <benoit.allard@gmx.de>
2019-12-08 16:30:03 +0100

    Day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 use std::io::{BufRead, BufReader};
 use std::fs::File;
 
@@ -27,8 +27,11 @@ fn nearest_intersection(path1: Vec<&str>, path2: Vec<&str>) -> i32{
   let points1 = gather_points(path1);
   let points2 = gather_points(path2);
   let mut min = std::i32::MAX;
-  for point in points1.intersection(&points2){
-      let distance = point.x.abs() + point.y.abs();
+  for (point, length) in &points1 {
+      if !points2.contains_key(point){
+          continue;
+      }
+      let distance = length + points2[point];
       if distance < min {
           min = distance;
       }
@@ -36,50 +39,55 @@ fn nearest_intersection(path1: Vec<&str>, path2: Vec<&str>) -> i32{
   return min;
 }
 
-fn gather_points(path: Vec<&str>) -> HashSet<Point>{
-   let mut points = HashSet::new();
+fn gather_points(path: Vec<&str>) -> HashMap<Point, i32>{
+   let mut points = HashMap::new();
    let mut current = Point{x: 0, y: 0};
+   let mut length = 0;
    for part in &path {
        let amount = part[1..].trim().parse().expect(part);
        match &part[0..1]{
-           "U" => up(&mut current, amount, &mut points),    
-           "D" => down(&mut current, amount, &mut points),
-           "L" => left(&mut current, amount, &mut points),    
-           "R" => right(&mut current, amount, &mut points),
+           "U" => up(&mut current, amount, &mut length, &mut points),    
+           "D" => down(&mut current, amount, &mut length, &mut points),
+           "L" => left(&mut current, amount, &mut length, &mut points),    
+           "R" => right(&mut current, amount, &mut length, &mut points),
            _ => return points,
        }
    }
    return points;
 }
 
-fn up(current: &mut Point, amount: i32, array: &mut HashSet<Point>){
+fn up(current: &mut Point, amount: i32, length: &mut i32, array: &mut HashMap<Point, i32>){
     for _i in 1..amount+1 {
         current.y += 1;
-        array.insert(Point{x: current.x, y: current.y});
+        *length += 1;
+        array.entry(Point{x: current.x, y: current.y}).or_insert(*length);
     }
 }
-fn down(current: &mut Point, amount: i32, array: &mut HashSet<Point>){
+fn down(current: &mut Point, amount: i32, length: &mut i32, array: &mut HashMap<Point, i32>){
     for _i in 1..amount+1 {
         current.y -= 1;
-        array.insert(Point{x: current.x, y: current.y});
+        *length += 1;
+        array.entry(Point{x: current.x, y: current.y}).or_insert(*length);
     }
 }
-fn left(current: &mut Point, amount: i32, array: &mut HashSet<Point>){
+fn left(current: &mut Point, amount: i32, length: &mut i32, array: &mut HashMap<Point, i32>){
     for _i in 1..amount+1 {
         current.x -= 1;
-        array.insert(Point{x: current.x, y: current.y});
+        *length += 1;
+        array.entry(Point{x: current.x, y: current.y}).or_insert(*length);
     }
 }
-fn right(current: &mut Point, amount: i32, array: &mut HashSet<Point>){
+fn right(current: &mut Point, amount: i32, length: &mut i32, array: &mut HashMap<Point, i32>){
     for _i in 1..amount+1 {
         current.x += 1;
-        array.insert(Point{x: current.x, y: current.y});
+        *length += 1;
+        array.entry(Point{x: current.x, y: current.y}).or_insert(*length);
     }
 }
 
 #[test]
 fn examples(){
-    assert_eq!(nearest_intersection(vec!["R8", "U5", "L5", "D3"], vec!["U7", "R6", "D4", "L4"]), 6);
-    assert_eq!(nearest_intersection(vec!["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"], vec!["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"]), 159);
-    assert_eq!(nearest_intersection(vec!["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"], vec!["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"]), 135);
+    assert_eq!(nearest_intersection(vec!["R8", "U5", "L5", "D3"], vec!["U7", "R6", "D4", "L4"]), 30);
+    assert_eq!(nearest_intersection(vec!["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"], vec!["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"]), 610);
+    assert_eq!(nearest_intersection(vec!["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"], vec!["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"]), 410);
 }
