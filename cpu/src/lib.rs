2a24df2211e62afd2dbc0224d1c62478dee61456
Benoît Allard <benoit.allard@gmx.de>
2020-01-04 16:04:05 +0100

    Day5-1 compiles, tests don't pass

--- cpu/src/lib.rs
+++ cpu/src/lib.rs
@@ -1,40 +1,85 @@
-struct CPU{
+#[derive(Default)]
+pub struct CPU{
     mem: Vec<i32>,
-    ip: usize
+    ip: usize,
+    inputs: Vec<i32>,
+    output: i32,
 }
 
 impl CPU {
     pub fn new(mem: &[i32]) -> CPU{
         CPU{
             mem: mem.to_vec(),
-            ip: 0
+            ip: 0,
+            ..Default::default()
         }
+    }
+
+    pub fn feed(&mut self, input: i32){
+        self.inputs.push(input);
+    }
+
+    pub fn starve(&self) -> i32{
+        return self.output;
     }
 
     pub fn run(&mut self){
-    loop {
-        let res;
-        match self.mem[self.ip] {
-            1 => res = self.arg(1) + self.arg(2),
-            2 => res = self.arg(1) * self.arg(2),
-            99 => return,
-            _ => panic!(self.mem[self.ip]),
+        loop {
+            match self.read(self.ip) % 100 {
+                1 => {
+                    let res = self.arg(1) + self.arg(2);
+                    self.write(self.read(self.ip + 3) as usize, res);
+                    self.ip += 4;
+                },
+                2 => {
+                    let res = self.arg(1) * self.arg(2);
+                    self.write(self.read(self.ip + 3) as usize, res);
+                    self.ip += 4;
+                },
+                3 => {
+                    let val = self.input();
+                    self.write(self.arg(1) as usize, val);
+                    self.ip += 2;
+                },
+                4 => {
+                    self.output(self.arg(1));
+                    self.ip += 2;
+                },
+                99 => return,
+                _ => panic!(self.read(self.ip)),
+            }
         }
-        let residx = self.mem[self.ip + 3] as usize;
-        self.mem[residx] = res;
-        self.ip += 4;
-    }
     }
 
-    fn arg(&mut self, offset: usize) -> i32{
-        return self.mem[self.mem[self.ip + offset] as usize];
+    fn arg(&self, offset: usize) -> i32{
+        return self.read(self.read(self.ip + offset) as usize);
+    }
+    
+    fn input(&mut self) -> i32{
+        return self.inputs.remove(0);
+    }
+
+    fn output(&mut self, value: i32){
+        println!("output: {}", value);
+        self.output = value
+    }
+
+
+    fn read(&self, addr: usize) -> i32{
+        return self.mem[addr];
+    }
+
+    fn write(&mut self, addr: usize, value: i32){
+        self.mem[addr] = value;
     }
 }
 
-pub fn process(program: &mut Vec<i32>){
+pub fn process(program: &mut Vec<i32>) -> i32{
     let mut cpu = CPU::new(program);
     cpu.run();
+    let res = cpu.starve();
     *program = cpu.mem;
+    return res;
 }
 
 #[test]
@@ -56,4 +101,23 @@ fn examples(){
     process(&mut pgm);
     assert_eq!(pgm[0], 30);
     assert_eq!(pgm[4], 2);
+}
+
+#[test]
+fn io(){
+    let mut cpu = CPU::new(&[3,0,4,0,99]);
+    cpu.feed(42);
+    cpu.run();
+    assert_eq!(cpu.starve(), 42);
+}
+
+#[test]
+fn mode(){
+    process(&mut vec![1002, 4, 3, 4, 33]);
+
+}
+
+#[test]
+fn neg(){
+    process(&mut vec![1101, 100, -1, 4, 0]);
 }
