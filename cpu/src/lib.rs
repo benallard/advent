adbaf2d2c7e8955eb600bfc809e0988805eee7ee
Benoît Allard <benoit.allard@gmx.de>
2020-01-12 17:25:47 +0100

    rustfmt, Day8-2

--- cpu/src/lib.rs
+++ cpu/src/lib.rs
@@ -1,5 +1,5 @@
 #[derive(Default)]
-pub struct CPU{
+pub struct CPU {
     mem: Vec<i32>,
     ip: usize,
     inputs: Vec<i32>,
@@ -8,52 +8,52 @@ pub struct CPU{
 }
 
 impl CPU {
-    pub fn new(mem: &[i32]) -> CPU{
-        CPU{
+    pub fn new(mem: &[i32]) -> CPU {
+        CPU {
             mem: mem.to_vec(),
             ip: 0,
             ..Default::default()
         }
     }
 
-    pub fn feed(&mut self, input: i32){
+    pub fn feed(&mut self, input: i32) {
         self.inputs.push(input);
     }
 
-    pub fn starve(&self) -> i32{
+    pub fn starve(&self) -> i32 {
         return self.output;
     }
 
-    pub fn done(&self) -> bool{
+    pub fn done(&self) -> bool {
         self.done
     }
 
-    pub fn run(&mut self){
+    pub fn run(&mut self) {
         loop {
             match self.opcode() {
                 1 => {
                     let res = self.get(1) + self.get(2);
                     self.put(3, res);
                     self.ip += 4;
-                },
+                }
                 2 => {
                     let res = self.get(1) * self.get(2);
                     self.put(3, res);
                     self.ip += 4;
-                },
+                }
                 3 => {
-                    if self.inputs.is_empty(){
+                    if self.inputs.is_empty() {
                         println!("Input required");
                         break;
                     }
                     let val = self.input();
                     self.put(1, val);
                     self.ip += 2;
-                },
+                }
                 4 => {
                     self.output(self.get(1));
                     self.ip += 2;
-                },
+                }
                 5 => {
                     let val = self.get(1);
                     if val != 0 {
@@ -61,7 +61,7 @@ impl CPU {
                     } else {
                         self.ip += 3;
                     }
-                },
+                }
                 6 => {
                     let val = self.get(1);
                     if val == 0 {
@@ -69,33 +69,33 @@ impl CPU {
                     } else {
                         self.ip += 3;
                     }
-                },
+                }
                 7 => {
-                    if self.get(1) < self.get(2){
+                    if self.get(1) < self.get(2) {
                         self.put(3, 1);
                     } else {
                         self.put(3, 0);
                     }
                     self.ip += 4;
-                },
+                }
                 8 => {
-                    if self.get(1) == self.get(2){
+                    if self.get(1) == self.get(2) {
                         self.put(3, 1);
                     } else {
                         self.put(3, 0);
                     }
                     self.ip += 4;
-                },
+                }
                 99 => {
                     self.done = true;
                     break;
-                },
+                }
                 _ => panic!(self.read(self.ip)),
             }
         }
     }
 
-    fn opcode(&self)-> u16{
+    fn opcode(&self) -> u16 {
         return (self.read(self.ip) % 100) as u16;
     }
 
@@ -104,45 +104,44 @@ impl CPU {
         return (val / 10i32.pow((offset - 1) as u32) % 10) as u8;
     }
 
-    fn get(&self, offset: usize) -> i32{
+    fn get(&self, offset: usize) -> i32 {
         let value = self.read(self.ip + offset);
-        return match self.mode(offset){
-            0=> self.read(value as usize),
-            1=> value,
+        return match self.mode(offset) {
+            0 => self.read(value as usize),
+            1 => value,
             _ => panic!(self.read(self.ip)),
         };
     }
 
-    fn put(&mut self, offset: usize, value: i32){
+    fn put(&mut self, offset: usize, value: i32) {
         self.write(self.read(self.ip + offset) as usize, value);
     }
-    
-    fn input(&mut self) -> i32{
+
+    fn input(&mut self) -> i32 {
         //println!("Input is {}", self.inputs[0]);
         return self.inputs.remove(0);
     }
 
-    fn output(&mut self, value: i32){
+    fn output(&mut self, value: i32) {
         //println!("output: {}", value);
         self.output = value
     }
 
-    fn read(&self, addr: usize) -> i32{
+    fn read(&self, addr: usize) -> i32 {
         //println!("Reading {} @{}", self.mem[addr], addr);
         return self.mem[addr];
     }
 
-    fn write(&mut self, addr: usize, value: i32){
+    fn write(&mut self, addr: usize, value: i32) {
         //println!("Writing {} to {}", value, addr);
         self.mem[addr] = value;
     }
 }
 
-pub fn process(program: &mut Vec<i32>) -> i32{
+pub fn process(program: &mut Vec<i32>) -> i32 {
     let mut cpu = CPU::new(program);
     cpu.run();
     let res = cpu.starve();
     *program = cpu.mem;
     return res;
 }
-
--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Clone)]
 struct Layer {
     data: Vec<Vec<u8>>,
 }
@@ -7,7 +8,7 @@ impl Layer {
         assert_eq!(width * height, content.len());
         let mut data = Vec::new();
         let mut line = Vec::new();
-        for (i, c) in content.char_indices() {
+        for c in content.chars() {
             line.push(c.to_digit(10).unwrap() as u8);
             if line.len() == width {
                 assert_eq!(width, line.len());
@@ -18,17 +19,61 @@ impl Layer {
         assert_eq!(height, data.len());
         Layer { data: data }
     }
+    pub fn new_empty(width: usize, height: usize) -> Layer {
+        let mut data = Vec::new();
+        for _ in 0..height {
+            let mut line = Vec::new();
+            for _ in 0..width {
+                line.push(2);
+            }
+            data.push(line);
+        }
+        assert_eq!(height, data.len());
+        Layer { data: data }
+    }
 
-    pub fn count(&self, v: u8) -> u16{
+    pub fn count(&self, v: u8) -> u16 {
         let mut res = 0;
-        for line in &self.data{
-            for char in line{
-                if *char == v{
+        for line in &self.data {
+            for char in line {
+                if *char == v {
                     res += 1;
                 }
             }
         }
         return res;
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> u8 {
+        return self.data[y][x];
+    }
+
+    pub fn blend(&mut self, other: &Layer) {
+        for y in 0..self.data.len() {
+            for x in 0..self.data[y].len() {
+                let char = self.data[y][x];
+                if char != 2 {
+                    continue;
+                }
+                if other.get(x, y) != 2 {
+                    self.data[y][x] = other.get(x, y);
+                }
+            }
+        }
+    }
+
+    pub fn print(&self) {
+        for line in &self.data {
+            let mut s_line = String::new();
+            for char in line {
+                match char {
+                    0 => s_line.push_str("  "),
+                    1 => s_line.push_str("XX"),
+                    _ => panic!(),
+                }
+            }
+            println!("{}", &s_line);
+        }
     }
 }
 
@@ -60,16 +105,28 @@ impl Image {
     pub fn part1(&self) -> u16 {
         let mut idx = 0;
         let mut min: u16 = (self.width * self.height + 1) as u16;
-        for (i, layer) in self.layers.iter().enumerate(){
+        for (i, layer) in self.layers.iter().enumerate() {
             let count = layer.count(0);
-            println!("{}: {}", i, count);
-            if count < min{
+            if count < min {
                 min = count;
                 idx = i;
             }
         }
-        println!("Layer {} it is, {}, {}", idx, self.layers[idx].count(1), self.layers[idx].count(2));
+        println!(
+            "Layer {} it is, {}, {}",
+            idx,
+            self.layers[idx].count(1),
+            self.layers[idx].count(2)
+        );
         return self.layers[idx].count(1) * self.layers[idx].count(2);
+    }
+
+    pub fn part2(&self) -> Layer {
+        let mut res = Layer::new_empty(self.width, self.height);
+        for layer in &self.layers {
+            res.blend(&layer);
+        }
+        return res;
     }
 }
 
@@ -78,9 +135,16 @@ fn main() {
     let img = Image::new(content, 25, 6);
 
     println!("Part 1: {}", img.part1());
+
+    img.part2().print();
 }
 
 #[test]
 fn test() {
-    let img = Image::new("123456789012", 3, 2);
+    let img = Image::new("0222112222120000", 2, 2);
+    let res = img.part2();
+    assert_eq!(0, res.get(0, 0));
+    assert_eq!(1, res.get(1, 0));
+    assert_eq!(1, res.get(0, 1));
+    assert_eq!(0, res.get(1, 1));
 }
