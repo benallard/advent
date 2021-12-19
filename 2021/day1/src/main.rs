1bfcbf1b7d4ba880ae70dfc4947b28f8d212b267
Benoît Allard <benoit.allard@gmx.de>
2021-12-19 17:43:43 +0100

    Day1-2

--- 2021/day1/src/main.rs
+++ 2021/day1/src/main.rs
@@ -1,12 +1,19 @@
 use std::io::BufRead;
 
 fn main() {
-    let mut res = 0;
-    let mut prev = u64::max_value();
-    std::io::BufReader::new(std::io::stdin())
+    let res = std::io::BufReader::new(std::io::stdin())
     .lines()
     .map(|line| line.unwrap().trim().parse().unwrap())
-    .for_each(|n| {if n > prev { res += 1} prev = n;});
+    .collect::<Vec<u64>>()
+    // comment out the following lines to get the first part
+    .windows(3)
+    .map(|window| window[0] + window[1] + window[2])
+    .collect::<Vec<u64>>()
+    // end comment
+    .windows(2)
+    .fold(0, |acc, w| {
+        acc + (w[1] > w[0]) as u64
+    });
     println!("{}", res);
 
 }
