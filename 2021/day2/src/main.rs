b977bc44961ea582b600c5a075e1d0969e152ecc
Benoît Allard <benoit.allard@gmx.de>
2021-12-19 18:38:16 +0100

    day2

---
+++ 2021/day2/src/main.rs
@@ -0,0 +1,28 @@
+use std::io::BufRead;
+
+fn main() {
+    let mut aim = 0;
+    let mut pos = 0;
+    let mut depth = 0;
+    std::io::BufReader::new(std::io::stdin())
+    .lines()
+    .for_each(|line| {
+        let line = line.unwrap();
+        let orders: Vec<&str> = line.split(' ').collect();
+        let val = orders[1].parse::<u64>().unwrap();
+        match orders[0] {
+            "forward" => {
+                pos += val;
+                depth += aim * val;
+            },
+            "up" => {
+                aim -= val;
+            },
+            "down" => {
+                aim += val;
+            }
+            _ => panic!("Unknown order: {}", orders[0])
+        }
+    });
+    println!("{}", pos * depth)
+}
