7017ff37d54c8cb206420cfe724c383dc3b25755
Benoît Allard <benoit.allard@gmx.de>
2022-12-06 18:45:40 +0100

    Part 2

--- 2022/day6/src/main.rs
+++ 2022/day6/src/main.rs
@@ -2,9 +2,12 @@ use std::io::Read;
 
 fn main() {
     let mut content = String::new();
-    std::io::BufReader::new(std::io::stdin()).read_to_string(&mut content).unwrap();
+    std::io::BufReader::new(std::io::stdin())
+        .read_to_string(&mut content)
+        .unwrap();
     let pos = get_start(&content);
     println!("start: {}", pos);
+    println!("message: {}", get_message(&content));
 }
 
 fn get_start(stream: &str) -> usize {
@@ -20,14 +23,35 @@ fn get_start(stream: &str) -> usize {
                 && w[1] != w[3]
                 && w[2] != w[3]
         })
-        .unwrap() + 4
+        .unwrap()
+        + 4
+}
+
+fn get_message(stream: &str) -> usize {
+    stream
+        .chars()
+        .collect::<Vec<_>>()
+        .windows(14)
+        .map(|w| w.iter().collect::<std::collections::HashSet<_>>())
+        .position(|s| s.len() == 14)
+        .unwrap()
+        + 14
 }
 
 #[test]
-fn test_start(){
+fn test_start() {
     assert_eq!(7, get_start("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
     assert_eq!(5, get_start("bvwbjplbgvbhsrlpgdmjqwftvncz"));
     assert_eq!(6, get_start("nppdvjthqldpwncqszvftbrmjlhg"));
     assert_eq!(10, get_start("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
     assert_eq!(11, get_start("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+}
+
+#[test]
+fn test_message() {
+    assert_eq!(19, get_message("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+    assert_eq!(23, get_message("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+    assert_eq!(23, get_message("nppdvjthqldpwncqszvftbrmjlhg"));
+    assert_eq!(29, get_message("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+    assert_eq!(26, get_message("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
 }
