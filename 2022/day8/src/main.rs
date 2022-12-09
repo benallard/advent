932243f18e83008e34c6f4ef930987f24d52ff17
Benoît Allard <benoit.allard@sla.de>
2022-12-09 13:36:04 +0100

    day8-part1

---
+++ 2022/day8/src/main.rs
@@ -0,0 +1,78 @@
+use std::io::BufRead;
+
+fn main() {
+    let field: Vec<Vec<u8>> = std::io::BufReader::new(std::io::stdin())
+        .lines()
+        .map(|l| l.unwrap().trim().into())
+        .map(|l: String| {
+            l.chars()
+                .map(|c| c.to_digit(10).unwrap() as u8)
+                .collect::<Vec<_>>()
+        })
+        .collect();
+
+    let height = field.len();
+    let width = field[0].len();
+
+    // The border is visible
+    let mut sum = 2 * width + (2 * (height - 2));
+    for y in 1..height - 1 {
+        for x in 1..width - 1 {
+            if visible(&field, x, y) {
+                //dbg!((x, y));
+                sum += 1;
+            }
+        }
+    }
+    println!("visible: {}", sum);
+}
+
+fn visible(field: &Vec<Vec<u8>>, x: usize, y: usize) -> bool {
+    let self_height = field[y][x];
+    // top
+    let mut res = true;
+    for yy in 0..y {
+        if field[yy][x] >= self_height {
+            res = false;
+            //dbg!("top");
+            break;
+        }
+    }
+    if res {
+        return res;
+    }
+    // left
+    res = true;
+    for xx in 0..x {
+        if field[y][xx] >= self_height {
+            res = false;
+            //dbg!("left");
+            break;
+        }
+    }
+    if res {
+        return res;
+    }
+    // bottom
+    let mut res = true;
+    for yy in (y + 1)..field.len() {
+        if field[yy][x] >= self_height {
+            res = false;
+            //dbg!("bottom");
+            break;
+        }
+    }
+    if res {
+        return res;
+    }
+    // right
+    res = true;
+    for xx in (x + 1)..field[y].len() {
+        if field[y][xx] >= self_height {
+            res = false;
+            //dbg!("right");
+            break;
+        }
+    }
+    return res;
+}
