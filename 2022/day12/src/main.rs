2860ee7055c0434b25cf1bf79db67399700db4c8
Benoît Allard <benoit.allard@gmx.de>
2022-12-12 19:52:47 +0100

    format, add comments

--- 2022/day12/src/main.rs
+++ 2022/day12/src/main.rs
@@ -30,52 +30,76 @@ fn main() {
     dist[start.y][start.x] = 0;
 
     // the points to process
-    let mut todo = vec![];
-    todo.push(start);
-    while let Some(point) = todo.pop() {
+    let mut todo = std::collections::VecDeque::new();
+    todo.push_back(start);
+    while let Some(point) = todo.pop_front() {
+        // new dist
         let res = dist[point.y][point.x] + 1;
+        // local height
         let local = field[point.y][point.x];
         // left
         if point.x > 0 {
-            let dest = Point{x: point.x-1, y: point.y};
+            let dest = Point {
+                x: point.x - 1,
+                y: point.y,
+            };
+            // shorter dist
             if dist[dest.y][dest.x] > res {
+                // accessible
                 if field[dest.y][dest.x] <= local + 1 {
+                    // won: update dist
                     dist[dest.y][dest.x] = res;
-                    todo.push(dest);
+                    // process point.
+                    todo.push_back(dest);
                 }
             }
         }
         // up
         if point.y > 0 {
-            let dest = Point{x: point.x, y: point.y-1};
+            let dest = Point {
+                x: point.x,
+                y: point.y - 1,
+            };
             if dist[dest.y][dest.x] > res {
                 if field[dest.y][dest.x] <= local + 1 {
                     dist[dest.y][dest.x] = res;
-                    todo.push(dest);
+                    todo.push_back(dest);
                 }
             }
         }
         // right
-        if point.x < field[point.y].len()-1 {
-            let dest = Point{x: point.x+1, y: point.y};
+        if point.x < field[point.y].len() - 1 {
+            let dest = Point {
+                x: point.x + 1,
+                y: point.y,
+            };
             if dist[dest.y][dest.x] > res {
                 if field[dest.y][dest.x] <= local + 1 {
                     dist[dest.y][dest.x] = res;
-                    todo.push(dest);
+                    todo.push_back(dest);
                 }
             }
         }
         // down
-        if point.y < field.len()-1 {
-            let dest = Point{x: point.x, y: point.y+1};
+        if point.y < field.len() - 1 {
+            let dest = Point {
+                x: point.x,
+                y: point.y + 1,
+            };
             if dist[dest.y][dest.x] > res {
                 if field[dest.y][dest.x] <= local + 1 {
                     dist[dest.y][dest.x] = res;
-                    todo.push(dest);
+                    todo.push_back(dest);
                 }
             }
         }
     }
+
+    dist.iter().for_each(|l| {
+        l.iter().for_each(|d| print!(" {:#4}", d));
+        println!("");
+    });
+
     println!("part1: {}", dist[end.y][end.x]);
 }
 
