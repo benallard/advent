e1ba2350a8b6d1ed2339f592a092d581fdd90b64
Benoît Allard <benoit.allard@gmx.de>
2022-12-14 19:44:08 +0100

    getting somewhere.

--- 2022/day13/src/main.rs
+++ 2022/day13/src/main.rs
@@ -1,6 +1,6 @@
 use std::{io::BufRead, str::FromStr};
 
-fn main(){
+fn main() {
     let part1 = std::io::BufReader::new(std::io::stdin())
         .lines()
         .map(|l| l.unwrap().trim().into())
@@ -8,35 +8,57 @@ fn main(){
         .chunks(3)
         .map(|c| PacketPair::new(&c[0], &c[1]))
         .enumerate()
-        .filter(|(i, pp)| pp.is_sorted())
-        .map(|(i, pp)| i)
+        .filter(|(_, pp)| pp.is_sorted())
+        .map(|(i, _)| i)
         .sum::<usize>();
     println!("count: {}", part1);
 }
 
-struct Packet{
-
+enum PacketValue {
+    Value(u32), // terminal
+    Deeper(Packet),
 }
 
-impl FromStr for Packet{
+struct Packet {
+    value: Vec<PacketValue>,
+}
+
+impl FromStr for Packet {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!()
+        // first and last char must be '[', ']'
+        if !s.starts_with('[') {
+            return Err("Wrong packet start".to_owned());
+        }
+        if !s.ends_with(']') {
+            return Err("Wrong Packet end".to_owned());
+        }
+        let value = s[1..s.len() - 1]
+            .split(",")
+            .map(|v| match v.parse() {
+                Ok(x) => PacketValue::Value(x),
+                Err(_) => PacketValue::Deeper(v.parse::<Packet>().unwrap()),
+            })
+            .collect();
+        Ok(Packet { value })
     }
 }
 
-struct PacketPair{
+struct PacketPair {
     packet1: Packet,
     packet2: Packet,
 }
 
-impl PacketPair{
-    fn new(l1: &str, l2: &str)-> PacketPair{
-        todo!()
+impl PacketPair {
+    fn new(l1: &str, l2: &str) -> PacketPair {
+        PacketPair {
+            packet1: l1.parse().unwrap(),
+            packet2: l2.parse().unwrap(),
+        }
     }
 
-    fn is_sorted(&self) -> bool{
+    fn is_sorted(&self) -> bool {
         todo!()
     }
 }
