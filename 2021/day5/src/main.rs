371443249ab203a088dce38830076037f8d787be
Benoît Allard <benoit.allard@gmx.de>
2023-01-15 14:09:24 +0100

    2021 day5 part2 - arithmetic stuff.

--- 2021/day5/src/main.rs
+++ 2021/day5/src/main.rs
@@ -10,18 +10,28 @@ fn main() {
         .collect::<Vec<_>>();
 
     let mut count = BTreeMap::new();
-    for line in lines {
+    for line in &lines {
         if line.is_diag() {
             println!("Skipping line");
             continue;
         }
         for point in line.points() {
-            dbg!(&point);
+            //dbg!(&point);
             *count.entry(point).or_insert(0) += 1;
         }
     }
     let part1 = count.iter().filter(|(_, v)| **v > 1).count();
     println!("part1: {}", part1);
+
+    let mut count = BTreeMap::new();
+    for line in &lines {
+        for point in line.points() {
+            //dbg!(&point);
+            *count.entry(point).or_insert(0) += 1;
+        }
+    }
+    let part2= count.iter().filter(|(_, v)| **v > 1).count();
+    println!("part2: {}", part2);
 }
 
 #[derive(Eq, Ord, PartialEq, PartialOrd, Debug)]
@@ -65,21 +75,38 @@ impl Line {
         !(self.a.x == self.b.x || self.a.y == self.b.y)
     }
 
+    fn len(&self) -> usize {
+        let x_len = self.a.x.abs_diff(self.b.x);
+        if x_len != 0 {
+            return x_len as usize;
+        }
+        self.a.y.abs_diff(self.b.y) as usize
+    }
+
     fn points(&self) -> Vec<Point> {
         let mut res = vec![];
-        let mut x_range = self.a.x..=self.b.x;
-        if x_range.is_empty(){
-            x_range = self.b.x..=self.a.x;
-        }
-        
-        for x in x_range {
-            let mut y_range = self.a.y..=self.b.y;
-        if y_range.is_empty(){
-            y_range = self.b.y..=self.a.y;
-        }
-            for y in y_range {
-                res.push(Point { x, y });
+        let x_offs:i32;
+        if self.a.x == self.b.x {
+            x_offs = 0;
+        } else if self.a.x > self.b.x{
+                x_offs = -1;
+            } else {
+                x_offs = 1;
             }
+        
+        let y_offs:i32;
+        if self.a.y == self.b.y {
+            y_offs = 0;
+        } else if self.a.y > self.b.y {
+            y_offs = -1;
+        } else {
+            y_offs = 1;
+        }
+        for i in 0..=self.len() {
+            res.push(Point {
+                x: (self.a.x as i32 + i as i32 * x_offs) as u32,
+                y: (self.a.y as i32 + i as i32 * y_offs) as u32,
+            });
         }
         res
     }
@@ -90,7 +117,14 @@ fn test_diag() {
     let line: Line = "0,9 -> 5,9".parse().unwrap();
     assert_eq!(false, line.is_diag());
     assert_eq!(6, line.points().len());
-    assert!( "6,4 -> 2,0".parse::<Line>().unwrap().is_diag());
+    assert!("6,4 -> 2,0".parse::<Line>().unwrap().is_diag());
     assert_eq!(7, "9,4 -> 3,4".parse::<Line>().unwrap().points().len());
     assert_eq!(7, "4,9 -> 4,3".parse::<Line>().unwrap().points().len());
+}
+
+#[test]
+fn test_arith(){
+    let a: u32 = 534;
+    let b: u32 = 7784;
+    //let c= a - b;
 }
