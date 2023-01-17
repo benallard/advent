369ea5029c3f86115130c1fa57a62367226f61ef
Benoît Allard <benoit.allard@gmx.de>
2023-01-17 21:05:13 +0100

    test is passing, not challenge

--- 2021/day8/src/main.rs
+++ 2021/day8/src/main.rs
@@ -9,11 +9,16 @@ fn main() {
         .map(|l| l.unwrap().trim().parse::<Entry>().unwrap())
         .collect::<Vec<_>>();
 
-    let part1 = entries.into_iter().map(|e| e.count_output_1_4_7_8()).sum::<usize>();
+    let part1 = entries
+        .iter()
+        .map(|e| e.count_output_1_4_7_8())
+        .sum::<usize>();
     println!("part1: {}", part1);
+    let part2 = entries.into_iter().map(|e| e.decode()).sum::<u32>();
+    println!("part2: {}", part2);
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 struct Digit {
     a: bool,
     b: bool,
@@ -81,7 +86,55 @@ impl Digit {
     fn is_1_4_7_or_8(&self) -> bool {
         match self.count() {
             2 | 3 | 4 | 7 => true,
-            _=> false,
+            _ => false,
+        }
+    }
+
+    fn and(&self, other: &Digit) -> Digit {
+        Digit {
+            a: self.a && other.a,
+            b: self.b && other.b,
+            c: self.c && other.c,
+            d: self.d && other.d,
+            e: self.e && other.e,
+            f: self.f && other.f,
+            g: self.g && other.g,
+        }
+    }
+
+    /*
+        0 0 | 0
+        0 1 | 0
+        1 0 | 1
+        1 1 | 0
+    */
+    fn minus(&self, other: &Digit) -> Digit {
+        Digit {
+            a: self.a && !other.a,
+            b: self.b && !other.b,
+            c: self.c && !other.c,
+            d: self.d && !other.d,
+            e: self.e && !other.e,
+            f: self.f && !other.f,
+            g: self.g && !other.g,
+        }
+    }
+
+    /*
+        0 0 | 0
+        0 1 | 1
+        1 0 | 1
+        1 1 | 1
+    */
+    fn plus(&self, other: &Digit) -> Digit {
+        Digit {
+            a: self.a || other.a,
+            b: self.b || other.b,
+            c: self.c || other.c,
+            d: self.d || other.d,
+            e: self.e || other.e,
+            f: self.f || other.f,
+            g: self.g || other.g,
         }
     }
 }
@@ -114,8 +167,112 @@ impl FromStr for Entry {
     }
 }
 
-impl Entry{
-    fn count_output_1_4_7_8(&self) -> usize{
+impl Entry {
+    fn count_output_1_4_7_8(&self) -> usize {
         self.output.iter().filter(|d| d.is_1_4_7_or_8()).count()
+    }
+
+    fn decode(&self) -> u32 {
+        let one = self
+            .patterns
+            .iter()
+            .filter(|d| d.count() == 2)
+            .next()
+            .unwrap();
+        let four = self
+            .patterns
+            .iter()
+            .filter(|d| d.count() == 4)
+            .next()
+            .unwrap();
+        let seven = self
+            .patterns
+            .iter()
+            .filter(|d| d.count() == 3)
+            .next()
+            .unwrap();
+        let eight = self
+            .patterns
+            .iter()
+            .filter(|d| d.count() == 7)
+            .next()
+            .unwrap();
+        // 7 & 1 -> top
+        let top = seven.and(one);
+        // (5&2&3) & 4 -> middle
+        let five_two_three = self
+            .patterns
+            .iter()
+            .filter(|d| d.count() == 5)
+            .cloned()
+            .reduce(|accum, item| accum.and(&item))
+            .unwrap();
+        let middle = five_two_three.and(four);
+        // 8 - middle -> 0
+        let zero = self
+            .patterns
+            .iter()
+            .filter(|d| d.count() == 6)
+            .filter(|d| eight.minus(d) == middle)
+            .next()
+            .unwrap();
+        // (5&2&3) - top - middle -> bottom
+        let bottom = five_two_three.minus(&top).minus(&middle);
+        // 3 = 1 + top + middle + bottom
+        let three = &one.plus(&top).plus(&middle).plus(&bottom);
+        // 9 = L6 - 3 = L1 -> TL
+        let nine = self
+            .patterns
+            .iter()
+            .filter(|d| d.count() == 6)
+            //.inspect(|d| println!("len: {}", d.minus(&three).count()))
+            .filter(|d| d.minus(&three).count() == 1)
+            .next()
+            .unwrap();
+        // 5 = L5 - top - middle - bottom - TL = L1
+        let five = self
+            .patterns
+            .iter()
+            .filter(|d| d.count() == 5)
+            .filter(|d| d.minus(&seven).minus(&three).count() == 1)
+            .next()
+            .unwrap();
+        // 2 = L5, !5, !3
+        let two = self
+            .patterns
+            .iter()
+            .filter(|d| d.count() == 5)
+            .filter(|d| d != &five)
+            .filter(|d| d != &three)
+            .next()
+            .unwrap();
+        // 6 = L6, !9 !0
+        let six = self
+            .patterns
+            .iter()
+            .filter(|d| d.count() == 6)
+            .filter(|d| d != &nine)
+            .filter(|d| d != &zero)
+            .next()
+            .unwrap();
+
+        let res = self
+            .output
+            .iter()
+            .map(|d| match d {
+                i if i == zero => 0,
+                i if i == one => 1,
+                i if i == two => 2,
+                i if i == three => 3,
+                i if i == four => 4,
+                i if i == five => 5,
+                i if i == six => 6,
+                i if i == seven => 7,
+                i if i == eight => 8,
+                i if i == nine => 9,
+                _ => panic!(),
+            })
+            .collect::<Vec<_>>();
+        res[0] * 1000 + res[1] * 100 + res[2] * 10 + res[3]
     }
 }
