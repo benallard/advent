16e8318925fb95aea9c8837061aebf3b0d838324
Benoît Allard <benoit.allard@gmx.de>
2023-01-25 20:59:19 +0100

    success

--- 2021/day9/src/main.rs
+++ 2021/day9/src/main.rs
@@ -5,7 +5,12 @@ fn main() {
     std::io::stdin().read_to_string(&mut buf).unwrap();
     let map: Map = buf.parse().unwrap();
 
-    let part1 = map.low_points().iter().map(|v| (v + 1) as u32).sum::<u32>();
+    let part1 = map
+        .low_points()
+        .iter()
+        //.inspect(|c| println!("{}, {}: {}", c.x, c.y, map.points[c.y][c.x]))
+        .map(|c| (map.points[c.y][c.x] + 1) as u32)
+        .sum::<u32>();
     println!("part1: {}", part1);
 }
 
@@ -13,12 +18,18 @@ struct Map {
     points: Vec<Vec<u8>>,
 }
 
+struct Coord {
+    x: usize,
+    y: usize,
+}
+
 impl FromStr for Map {
     type Err = String;
     fn from_str(content: &str) -> Result<Self, <Self as FromStr>::Err> {
         Ok(Map {
             points: content
-                .split("\n")
+                .lines()
+                //.inspect(|l| println!("line: {}", l))
                 .map(|l| {
                     l.chars()
                         .map(|c| c.to_digit(10))
@@ -27,13 +38,14 @@ impl FromStr for Map {
                         .collect()
                 })
                 .filter(|v: &Vec<u8>| v.len() != 0)
+                //.inspect(|v| println!("len: {}", v.len()))
                 .collect(),
         })
     }
 }
 
 impl Map {
-    fn low_points(&self) -> Vec<u8> {
+    fn low_points(&self) -> Vec<Coord> {
         self.points
             .iter()
             .enumerate()
@@ -41,7 +53,7 @@ impl Map {
                 l.iter()
                     .enumerate()
                     .filter(|(x, _)| self.low_point(*x, y))
-                    .map(|(_, v)| *v)
+                    .map(|(x, _)| Coord { x, y })
                     .collect::<Vec<_>>()
             })
             .collect()
@@ -49,6 +61,9 @@ impl Map {
 
     fn low_point(&self, x: usize, y: usize) -> bool {
         let val = self.points[y][x];
+        if val == 9 {
+            return false;
+        }
         if x != 0 && self.points[y][x - 1] < val {
             return false;
         }
