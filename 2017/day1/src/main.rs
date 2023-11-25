528912e2f9e372a9f60fcd0832365cdc7921e0c0
Benoît Allard <benoit.allard@gmx.de>
2023-11-25 11:50:06 +0100

    some progress day1_17

--- 2017/day1/src/main.rs
+++ 2017/day1/src/main.rs
@@ -1,9 +1,19 @@
-fn main(){
+use std::io;
 
+fn main(){
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    println!("Part1: {}", captcha(&input))
 }
 
-fn captcha(_val: &str) -> i32 {
-    0
+fn captcha(val: &str) -> u32 {
+    //val.push(val.chars().nth(0));
+    val.chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect::<Vec<u32>>()
+        .windows(2)
+        .map(|w| if w[0] == w[1] {w[0]} else {0})
+        .sum::<u32>()
 }
 
 #[test]
