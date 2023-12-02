8ac1968eed3a646a1e9038e5d67a401aa8d3fd8e
Benoît Allard <benoit.allard@gmx.de>
2023-12-02 15:46:50 +0100

    day2-part2: ok

--- 2023/day2/main.kts
+++ 2023/day2/main.kts
@@ -1,4 +1,5 @@
 #!/usr/bin/env -S kotlinc -J-ea -script
+println("ready")
 
 class Game(val id: Int, val sets: Set<Map<String, Int>>){
 
@@ -7,6 +8,24 @@ class Game(val id: Int, val sets: Set<Map<String, Int>>){
         return sets.all{ it.get("red") ?: 0 <= 12
          && it.get("green")?: 0 <= 13
          && it.get("blue")?: 0 <= 14}
+    }
+
+    fun power(): Int {
+        var red = 0;
+        var green = 0;
+        var blue = 0;
+        for (set_ in sets){
+            if (red < set_.get("red") ?: 0){
+                red = set_.get("red")?: 0
+            }
+            if (green < set_.get("green") ?: 0){
+                green = set_.get("green")?: 0
+            }
+            if (blue < set_.get("blue") ?: 0){
+                blue = set_.get("blue")?: 0 
+            }
+        }
+        return red * green * blue;
     }
 
 }
@@ -34,13 +53,23 @@ assert(!readGame("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 gr
 assert(!readGame("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red").possible())
 assert(readGame("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green").possible())
 
+assert(readGame("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green").power() == 48)
+assert(readGame("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue").power() == 12)
+assert(readGame("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red").power() == 1560)
+assert(readGame("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red").power() == 630)
+assert(readGame("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green").power() == 36)
+
+
 
 var part1 = 0
+var part2 = 0
 
 for (line in generateSequence{readLine()}){
     val game = readGame(line)
     if (game.possible()){
         part1 += game.id;
     }
+    part2 += game.power();
 }
 println("Part1: $part1")
+println("Part2: $part2")
