4fd14a6296fba9999b3b60cf0089ca98db5899a9
Benoît Allard <benoit.allard@gmx.de>
2023-12-12 19:12:25 +0100

    day9, part 2 ok

--- 2023/day9.kts
+++ 2023/day9.kts
@@ -1,22 +1,35 @@
 #!/usr/bin/env -S kotlinc -J-ea -script
 println("ready")
 
-fun predict(values: List<Int>): Int {
-    if (values.all{it == 0}){
+fun predict1(values: List<Int>): Int {
+    if (values.all { it == 0 }) {
         return 0
     }
-    return values.last() + predict(values.windowed(2)
+    return values.last() + predict1(values.windowed(2)
             .map { w -> w[1] - w[0] })
 }
 
-val res1 = predict(listOf(0,3,6,9,12,15))
-assert(res1 == 18)
+fun predict2(values: List<Int>): Int {
+    if (values.all { it == 0 }) {
+        return 0
+    }
+    return values.first() - predict2(values.windowed(2)
+            .map { w -> w[1] - w[0] })
+}
 
-val part1 = generateSequence { readlnOrNull() }.map { it.split(" +".toRegex())
-        .map { it.toInt() }
-        .toList()}
-        .map{predict(it)}
-        .sum()
+val res1 = predict2(listOf(10, 13, 16, 21, 30, 45))
+assert(res1 == 5)
+
+
+val input = generateSequence { readlnOrNull() }.map {
+    it.split(" +".toRegex())
+            .map { it.toInt() }
+            .toList()
+}.toList()
+
+val part1 = input.sumOf { predict1(it) }
 println("Part1: $part1")
 
+val part2 = input.sumOf { predict2(it) }
+println("Part2: $part2")
 
