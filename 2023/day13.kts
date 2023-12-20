371167712142135c4da16c2c8d8de3996351ebf7
Benoît Allard <benoit.allard@gmx.de>
2023-12-20 21:00:30 +0100

    day13-part2: ok don't forget to skip the previous result

--- 2023/day13.kts
+++ 2023/day13.kts
@@ -20,19 +20,22 @@ enum class Spot(private val d: Char) {
 
 class MirrorMap(private val spots: List<List<Spot>>) {
     val valid = spots.map { it.size }.distinct().size == 1
-    fun part1(): Int {
-        val part1hor = part1hor()
+    fun part1(skip: Int = 0): Int {
+        val part1hor = part1hor(skip / 100)
         if (part1hor != 0) {
             println("Hor: $part1hor")
             return 100 * part1hor
         }
-        val ver = part1ver()
+        val ver = part1ver(skip)
         println("Ver: $ver")
         return ver
     }
 
-    private fun part1hor(): Int {
+    private fun part1hor(skip: Int = 0): Int {
         for (idx in 1..<spots.size) {
+            if (idx == skip){
+                continue
+            }
             var res = true
             var iidx = 0
             while (res && ((idx - iidx - 1) >= 0) && ((idx + iidx) < spots.size)) {
@@ -47,11 +50,15 @@ class MirrorMap(private val spots: List<List<Spot>>) {
         return 0
     }
 
-    private fun part1ver(): Int {
+    private fun part1ver(skip: Int = 0): Int {
         for (idx in 1..<spots[0].size) {
+            if (idx == skip){
+                continue
+            }
             var res = true
             var iidx = 0
             while (res && ((idx - iidx - 1) >= 0) && ((idx + iidx) < spots[0].size)) {
+                println("$idx, $iidx, ${idx - iidx - 1}, ${idx + iidx}, ${cmpver(idx - iidx - 1, idx + iidx)}")
                 res = res && cmpver(idx - iidx - 1, idx + iidx)
                 iidx += 1
             }
@@ -71,18 +78,19 @@ class MirrorMap(private val spots: List<List<Spot>>) {
     fun part2(): Int{
         val part1 = part1()
         for (map in mutate1()){
-            val res = map.part1()
+            val res = map.part1(part1)
             if (res != part1 && res != 0){
                 return res
             }
         }
+        println("None found for ${spots}")
         return 0
     }
 
     private fun mutate1(): Iterator<MirrorMap> = object : Iterator<MirrorMap> {
         var x = 0
         var y = 0
-        override fun hasNext(): Boolean = x < spots[0].size && y < spots.size
+        override fun hasNext(): Boolean = y != spots.size
         override fun next(): MirrorMap {
             println("Now at: $x, $y")
             val newSpots = spots.map { it.toMutableList() }.toMutableList()
@@ -91,7 +99,7 @@ class MirrorMap(private val spots: List<List<Spot>>) {
                 Spot.ASH -> Spot.ROCK
                 Spot.ROCK -> Spot.ASH
             }
-            println("$old -> ${newSpots[y][x]}")
+            //println("$old -> ${newSpots[y][x]}, (${spots[y][x]})")
             if (++x == spots[0].size) {
                 x = 0
                 y += 1
@@ -128,3 +136,13 @@ val part2 = maps
         .map { it.part2() }
         .sum()
 println("Part2: $part2")
+//
+//[[#.#.##..#.#..]
+// [.##.##...#.##]
+// [.######.....#]
+// [#....#..###..]
+// [#########.#..]
+// [...##.#.##...]
+// [#..#.###.####]
+// [#..#.###.####]
+// [...##.#.##...]]
