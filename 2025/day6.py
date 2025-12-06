5246b108de73d13281b83cd154bda4df5787196a
Benoît Allard <benoit.allard@gmx.de>
2025-12-06 17:21:00 +0100

    day6-part2: ok

--- 2025/day6.py
+++ 2025/day6.py
@@ -14,10 +14,30 @@ def evaluate(nums, op):
     print(f"Res: {res}")
     return res
 
+def processNumbers(input):
+    nums = []
+    op = None
+    sum = 0
+    for num in input:
+        if not num.strip():
+            sum += evaluate(nums, op)
+            nums = []
+            op = None
+            continue
+        print (f"'{num}'")
+        if num[-1] in ('+', '*'):
+            op = num[-1]
+            num = num[:-1]
+        nums.append(int(num.strip()))
+    sum += evaluate(nums, op)
+    return sum
+
+
 problems = []
+numbers = []
 sum = 0
 for line in sys.stdin:
-    line = line.strip()
+    line = line.rstrip()
     if not problems:
         problems = [[int(x)] for x in line.split()]
     else:
@@ -27,4 +47,19 @@ for line in sys.stdin:
             else:
                 problems[i].append(int(part))
 
+    # Part 2: We rotate the input anti-CW, one quarter, so we can read the numbers easily
+    if not numbers:
+        numbers = [char for char in line]
+        print(numbers)
+    else:
+        for i, char in enumerate(line):
+            if i >= len(numbers):
+                numbers.append(' ' * (len(numbers[0]) - 1)) # Take 0 as length reference
+            numbers[i] += char
+
+
 print(sum)
+
+
+print(numbers)
+print(f"Part 2: {processNumbers(numbers)}")
