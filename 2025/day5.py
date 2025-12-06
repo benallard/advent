5f74e9ee2438f3d7dd6b1eae3a53afad0c487bd3
Benoît Allard <benoit.allard@gmx.de>
2025-12-06 14:46:37 +0100

    day5-part2: nok: counts too many

--- 2025/day5.py
+++ 2025/day5.py
@@ -1,6 +1,5 @@
 import sys
 
-importfresh = True
 
 def isFresh(article):
     for start, end in fresh:
@@ -11,8 +10,42 @@ def isFresh(article):
         return True
     return False
 
+
+def countAllFresh():
+    count = 0
+    sortedFresh = sorted(fresh, key=lambda f: f[0])
+    for i, (start, end) in enumerate(sortedFresh[:-1]):
+        nextStart = sortedFresh[i + 1][0]
+        if nextStart < end:
+            # Only add till next Start, next range wil care of the rest
+            count += nextStart - start
+            print(f"Adding start: {start} - {nextStart}: {count}")
+        else:
+            # Disjointed with next one: add all, move along
+            count += end - start + 1
+            print(f"Adding all: {start} - {end}: {count}")
+            continue
+        # Now we should care of the fact that the next range(s!) might end before our own
+        lastEnd = end
+        for nextStart, nextEnd in sortedFresh[i + 1:]:
+            if nextEnd > end:
+                # Too far
+                break
+            if nextEnd < lastEnd:
+                lastEnd = nextEnd
+        if end != lastEnd:
+            print(f"Adding end: {lastEnd} - {end}: {count}")
+            count += end - lastEnd
+    # Dont forget the last range !
+    lastStart, lastEnd = sortedFresh[-1]
+    count += lastEnd - lastStart + 1
+    print(f"Adding last {lastStart} - {lastEnd}: {count}")
+    return count
+
+
+importfresh = True
+
 fresh = []
-max = 0
 
 countFresh = 0
 for line in sys.stdin:
@@ -24,8 +57,6 @@ for line in sys.stdin:
     if importfresh:
         start, end = map(int, line.split("-"))
         fresh.append((start, end))
-        if end > max:
-            max = end
     else:
         article = int(line)
         if isFresh(article):
@@ -36,9 +67,5 @@ for line in sys.stdin:
 
 print(f"{countFresh} fresh articles")
 
-allFresh = 0
-for article in range(max + 1):
-    if isFresh(article):
-        allFresh += 1
-
+allFresh = countAllFresh()
 print(f"{allFresh} posible fresh Articles")
