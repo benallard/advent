a6ebd6fb50db9c453a2b327cab0b2fad62533d5f
Benoît Allard <benoit.allard@gmx.de>
2025-12-07 13:48:55 +0100

    day7-part2: ok, at the end, we use dicts ;)

--- 2025/day7.py
+++ 2025/day7.py
@@ -7,20 +7,22 @@ for line in sys.stdin:
     grid.append(list(x for x in line))
 print(grid[0].index('S'))
 
-rays = [grid[0].index('S')]
+rays = {grid[0].index('S'): 1}
 
 numsplits = 0
 for line in grid[1:]:
-    newrays = list()
-    for ray in rays:
+    newrays = {}
+    for ray, weight in rays.items():
         if line[ray] == '.':
-            newrays.append(ray)
+            newrays[ray] = newrays.get(ray, 0) + weight
         if line[ray] == '^':
-            newrays.append(ray-1)
-            newrays.append(ray+1)
+            newrays[ray-1] = newrays.get(ray - 1, 0) + weight
+            newrays[ray + 1] = newrays.get(ray + 1, 0) + weight
             numsplits += 1
     rays = newrays
 
 print(len(rays))
 print(numsplits)
+
+print(f"Part2: {sum(rays.values())}")
 
