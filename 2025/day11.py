6be6f05f52576e277fc116c3b97f24246cbc1a47
Benoît Allard <benoit.allard@gmx.de>
2025-12-11 19:38:55 +0100

    day11-part2: nok, performance issues

--- 2025/day11.py
+++ 2025/day11.py
@@ -8,6 +8,9 @@ for line in sys.stdin:
     name, out = line.split(": ")
     devices[name] = set(out.split())
 
+# Make sure the out node is there
+if 'out' not in devices:
+    devices['out'] = {}
 
 def numpaths(start, end):
     sum = 0
@@ -22,18 +25,21 @@ def numpaths(start, end):
 if 'you' in devices:
     print(f"Part1: {numpaths('you', 'out')}")
 
+# Let's be smarter and reduce the search space:
+#start - fft - dac - end
+path1 = numpaths("svr", "fft")
+print(path1)
+path1 *= numpaths("fft", "dac")
+print(path1)
+path1 *= numpaths("dac", "out")
+print(path1)
 
-def paths(start, end, prefix=[]):
-    for next in devices[start]:
-        if next == end:
-            yield prefix + [next]
-        else:
-            yield from paths(next, end, prefix + [next])
+# start - dac - fft - end
+path2 = numpaths("svr", "dac")
+print(path2)
+path2 *= numpaths("dac", "fft")
+print(path2)
+path2 *= numpaths("fft", "out")
+print(path2)
 
-
-sum = 0
-for path in paths("svr", "out"):
-    if "fft" in path and "dac" in path:
-        sum += 1
-
-print(f"Part2: {sum}")
+print(f"Part2: {path1 + path2}")
